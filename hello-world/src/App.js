b2390eca85581773579bfdc279a518641b597cfb
unknown <kunalsharmarok@gmail.com>
2025-08-01 19:01:24 +0530

    forwarding Refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
-
+import FRParentInput from './components/FRParentInput';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      <FRParentInput/>
      
 
       
