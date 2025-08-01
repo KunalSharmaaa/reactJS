f64afc22568a793af55105d3dbefdb2acd20cb02
unknown <kunalsharmarok@gmail.com>
2025-08-01 18:26:35 +0530

    refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,29 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import inline from './components/inline';
-import './appStyles.css';
-import './components/myStyles.css';
-import styles from './appStyles.modules.css'
-import Form from './components/Form';
-import ChildComponent from './components/ChildComponent';
-import LifecycleA from './components/LifecycleA';
+import RefsDemo from './components/RefsDemo';
+
 
 function App() {
   return (
     <div className="App">
-      <LifecycleA/>
+      <RefsDemo/>
+     
 
       
     </div>
