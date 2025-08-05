faa8f863fc62c00768ac57521f6828d82cb406bc
unknown <kunalsharmarok@gmail.com>
2025-08-05 19:29:05 +0530

    useState Hook

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo/>
+      <HookCounterFour/>
     </div>
   );
 }
