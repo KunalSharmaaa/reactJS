b6a08b6c522a2055db33d5a33a55a70348f58d5a
unknown <kunalsharmarok@gmail.com>
2025-08-06 20:19:28 +0530

    useEffect hook

--- state-hook/src/components/ClassCounterOne.js
+++ state-hook/src/components/ClassCounterOne.js
@@ -5,20 +5,32 @@ class ClassCounterOne extends Component {
       super(props)
     
       this.state = {
-         count: 0
+        count: 0,
+        name: ''
       }
     }
 
     componentDidMount() {
         document.title = 'Clicked ${this.state.count} times'
     }
-    componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      console.log('Updating document title')
         document.title = `Clicked ${this.state.count} times`
+    }
     }
     
   render() {
     return (
-        <div>   
+      <div>   
+        <input
+          type='text'
+          value={this.state.name}
+          onChange={e => {
+            this.setState({ name: e.target.value })
+          }}
+              />
+
             <button onClick={() => this.setState({ count: this.state.count + 1 })}>
                 Click {this.state.count} times
             </button>
@@ -29,3 +41,4 @@ class ClassCounterOne extends Component {
 }
 
 export default ClassCounterOne
+
