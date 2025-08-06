1d0d839ef416a4911137c01e318409494203d779
unknown <kunalsharmarok@gmail.com>
2025-08-06 21:55:55 +0530

    useEffect with cleanup

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import ClassCounterOne from './components/ClassCounterOne';
 import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounterOne /> */}
-      <HookCounterOne/>
+      {/* <ClassMouse /> */}
+      {/* <HookMouse/> */}
+      <MouseContainer/>
     </div>
   );
 }
