f065641092665abe843ad1c30adc80b30462ba95
unknown <kunalsharmarok@gmail.com>
2025-08-09 19:55:44 +0530

    issue

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
+import React, {useReducer } from 'react';
 import './App.css';
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
+
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return state
+    default:
+      return state
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
+
+    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
     <div className="App">
-      <CounterTwo/>
-    </div>
+      <ComponentA />
+      <ComponentB />
+      <ComponentC/>
+      </div>
+      </CountContext.Provider>
   );
 }
 
