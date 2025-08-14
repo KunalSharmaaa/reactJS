ca317b606d9a5a4b696b687100706e192e0856d6
unknown <kunalsharmarok@gmail.com>
2025-08-14 15:32:28 +0530

    Fetching Data With UseReducer

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,13 +1,16 @@
-import React, {useReducer } from 'react';
+import React from 'react'
 import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 function App() {
   return (
 
   
     <div className="App">
+      <DataFetchingTwo/>
       </div>
-  );
+  );  
 }
 
 export default App;
