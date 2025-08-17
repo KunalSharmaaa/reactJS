b28a354dc71d14f1e469afb5f7f26dfef643013e
unknown <kunalsharmarok@gmail.com>
2025-08-17 13:10:27 +0530

    useRef hooks

---
+++ ref-hook/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassTimer />
+      <HookTimer/>
+    </div>
+  );
+}
+
+export default App;
